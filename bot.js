2aaa8aaa17247c14ed8567e7bff2df4ca8d1cdbf
Mirza Nor Azman <spellhaikarl@gmail.com>
2019-02-17 16:11:22 -0600

    implements reply to mentions, favorite and reblog

--- bot.js
+++ bot.js
@@ -12,16 +12,57 @@ const M = new Mastodon({
     api_url: 'https://botsin.space/api/v1/', // optional, defaults to https://mastodon.social/api/v1/
 })
 
-toot();
-setInterval(toot, 50000);
+// toot();
+// setInterval(toot, 50000);
 
+const listener = M.stream('streaming/user')
 
+listener.on('message', msg => {
+    // fs.writeFileSync(`data${new Date().getTime()}`, JSON.stringify(msg,null, 2));
+    // console.log('user event');
+    if (msg.event === 'notification'){
+        const acct = msg.data.account.acct;
+        if (msg.data.type === 'follow'){
+            toot(`@${acct} Yuk Follow`);
+        }
+        else if (msg.data.type === 'mention'){
+            const regex1 = /(like|favou?rite)/i;
+            const content = msg.data.status.content;
+            const id = msg.data.status.id;
+            console.log(`mention: ${id} ${content}`);
+            if(regex1.test(content)){
+                M.post(`statuses/${id}/favourite`, (error, data) => {
+                    if (error) console.error('error');
+                    else console.log(`Favourited ${id}`);
+                })
+            }
 
-function toot() {
-    const num = Math.floor(Math.random() * 100);
+            const regex2= /(boost|reblog|retweet)/i;
+            console.log(`mention: ${id} ${content}`);
+            if(regex2.test(content)){
+                M.post(`statuses/${id}/reblog`, (error, data) => {
+                    if (error) console.error('error');
+                    else console.log(`Reblog ${id}`);
+                })
+            }
+
+            const regex3 = /\?/;
+            if (regex3.test(content)){
+                console.log('I got a question');
+                const reply = `@${acct} That is a good question!`
+                toot(reply, id);
+            }
+        }
+    }
+});
+listener.on('error', err => console.log(err))
+
+function toot(content,id) {
     const params = {
-        spoiler_text: "The meaning of life: ",
-        status: num
+        status: content,
+    }
+    if(id){
+        params.in_reply_to_id = id;
     }
 
     M.post('statuses', params, (error,data) =>{
